00ab076ef25c76ea6544600cd57f753b77f4bfc1
SRINI-KS <sriniks887@gmail.com>
2022-05-23 09:56:11 +0530

    commit

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,5 @@
-const String appname = 'Free';
-const String slogan = 'Bruh';
+const String appname = 'Freelancer';
+const String slogan = '';
 const String bgimage = 'assets/vipappimage1.jpg';
 const String loginstring = 'LOGIN';
 const String forgettext = 'Forget Password?';
